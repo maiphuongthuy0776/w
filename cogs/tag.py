529b14cfb9fd42e0fd98a456e70fdeb425b33c99
maiphuongthuy0776 <mayukovn2001@gmail.com>
2026-06-12 13:31:46 +0700

    Add files via upload

---
+++ cogs/tag.py
@@ -0,0 +1,29 @@
+﻿import random
+import discord
+from discord.ext import commands
+
+class Tag(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.responses = [
+            "gì vậy em guột", 
+        ]
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.author.bot:
+            return
+        if message.author == self.bot.user:
+            return
+        if not message.guild:
+            return
+
+        if self.bot.user in message.mentions:
+            response = random.choice(self.responses)
+            try:
+                await message.reply(response)
+            except Exception:
+                pass
+
+async def setup(bot):
+    await bot.add_cog(Tag(bot))
